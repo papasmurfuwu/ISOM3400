bcb3c8b237a1d6ed9f4ad57cb0fdb29e4451bd55
papasmurfuwu <quentanwong@gmail.com>
2024-03-21 19:10:35 +0800

    Updated main to cookie clicker automation using Selenium

--- Selenium/main.py
+++ Selenium/main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time 
 
 def launchBrowser():
@@ -10,9 +12,44 @@ def launchBrowser():
     service = Service(executable_path="chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=options)
 
-    driver.get("https://google.com")
-    input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
-    input_element.send_keys("tech with tim" + Keys.ENTER)
-    return driver
+    driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+    cookie_id = "bigCookie"
+    cookies_id = "cookies"
+    product_price_prefix = "productPrice"
+    product_prefix = "product"
+
+    # Wait for language with XPATH "English" to pop up 
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
+    )
+    
+    language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
+    language.click()
+
+
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, cookie_id))
+    )
+    cookie = driver.find_element(By.ID, cookie_id)
+  
+    while True:
+        cookie.click()
+        cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
+        cookies_count = int(cookies_count.replace(",", ""))
+
+        for i in range(4):
+            product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+
+            if not product_price.isdigit():
+                continue
+
+            product_price = int(product_price)
+
+            if cookies_count >= product_price:
+                product = driver.find_element(By.ID, product_prefix + str(i))
+                product.click()
+                break
+
 
 launchBrowser()
